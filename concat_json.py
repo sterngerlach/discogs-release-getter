8ecf37f83c35ebdf93fa9ce6024ac96518033521
Stern Gerlach <std.experimental.optional@gmail.com>
2019-06-27 12:33:07 +0900

    add concat_json.py

---
+++ concat_json.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# concat_json.py
+
+import glob
+import json
+
+def main():
+    json_files = glob.glob("datasets/label_[0-9]*.json")
+    concat_data = []
+    
+    for file_name in json_files:
+        label_file = open(file_name, "r", encoding="utf-8")
+        label_data = json.load(label_file)[0]
+
+        for release in label_data["releases"]:
+            position = 1
+
+            for track in release["tracks"]:
+                track["position"] = position
+                position += 1
+
+            if release["rating_count"] == 0:
+                del release["rating_count"]
+                del release["rating"]
+
+        concat_data.append(label_data)
+        label_file.close()
+
+    file_name = "datasets/label.json"
+    label_file = open(file_name, "w", encoding="utf-8")
+    json.dump(concat_data, label_file, ensure_ascii=False, indent=4,
+              sort_keys=False, separators=(",", ": "))
+    label_file.close()
+
+if __name__ == "__main__":
+    main()
+
